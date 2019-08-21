c860cd491368eabfae9a8589eace2836071c71d3
zachattack531 <26945971+zachattack531@users.noreply.github.com>
2019-08-21 16:00:51 -0400

    Added ground/dinogame

--- dino-game/Ground.js
+++ dino-game/Ground.js
@@ -1,5 +1,15 @@
 class Ground {
     constructor() {
+        this.pos = createVector(width/2, height - height/4);
+    }
+    show(){
+        push();
+        noStroke();
+        translate(this.pos.x, this.pos.y);
+        fill(0);
+        rectMode(CENTER);
+        rect(0, 0, width, 3);
+        pop();
 
     }
 }
--- dino-game/sketch.js
+++ dino-game/sketch.js
@@ -1,7 +1,11 @@
-function setup() {
+let ground;
 
+function setup() {
+    createCanvas(640,480);
+    ground = new Ground();
 }
 
 function draw() {
-
+    background(255);
+    ground.show();
 }
