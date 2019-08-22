4aff6e481910ebe97352d3149058b8b2db978844
zachattack531 <26945971+zachattack531@users.noreply.github.com>
2019-08-22 15:52:22 -0400

    Neural net added, not implemented

--- dino-game/Network.js
+++ dino-game/Network.js
@@ -1,5 +1,53 @@
 class NeuralNetwork {
     constructor(a,b,c,d) {
-        
+        if(a instanceof tf.Sequential){
+            this.model = a;
+            this.inputNodes = b;
+            this.hiddenNodes = c;
+            this.outputNodes = d;
+        }   else {
+            this.inputNodes = a;
+            this.hiddenNodes = b;
+            this.outputNodes = c;
+            this.model = this.createModel()
+        }
+    }
+
+    createModel(){
+        //Unsupervised, dense neural network
+        const model = tf.sequential();
+        const hidden = tf.layers.dense({
+            units: this.hiddenNodes,
+            inputShape: [this.inputNodes],
+            activation: 'sigmoid'
+        })
+        model.add(hidden);
+        const hidden2 = tf.layers.dense({
+            units: this.hiddenNodes,
+            activation: 'sigmoid'
+        })
+        model.add(hidden2);
+        const output = tf.layers.dense({
+            units: this.outputNodes,
+            activation: 'softmax'
+        })
+        model.add(output);
+
+        return model;
+    }
+
+    dispose(){
+        this.model.dispose();
+    }
+    predict(inputs){
+        return tf.tidy(() =>{
+            const xs = tf.tensor2d([inputs]);
+
+            const ys = this.model.predict(xs); 
+
+            const output = ys.dataSync();
+
+            return output  // convert tensor to array
+        }); 
     }
 }
