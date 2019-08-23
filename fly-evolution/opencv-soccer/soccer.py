c6e5ef57642cb8db36d754351b3c4f3f1ebfb98e
Andrew Pasquale <andrew@elytra.net>
2019-08-23 14:48:18 -0400

    Add camera setup and main loop

--- fly-evolution/opencv-soccer/soccer.py
+++ fly-evolution/opencv-soccer/soccer.py
@@ -7,3 +7,19 @@ def overlap(ballX, ballY, ballRadius, goalX, goalY, goalWidth, goalHeight):
             return True
     return False
 
+cap = cv2.VideoCapture(1)
+cap.set(3, 640)
+cap.set(4, 480)
+
+while True:
+
+    _, img = cap.read()
+
+    img = cv2.flip(img, 1)
+
+
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cap.release()
+        cv2.destroyAllWindows()
+        break
